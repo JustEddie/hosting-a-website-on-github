5db5deffaa157cbde7d79fa206aac0892ad51937
JustEddie <skdpwls830@gmail.com>
2022-07-05 01:47:31 +1000

    add JS but need change, fixed thor image, change about me layout

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
-document.querySelector('body').appendChild(h2);
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop < 50) {
+    document.getElementById("divHead").style.fontSize = "40px";
+  } else {
+    document.getElementById("divHead").style.fontSize = "20px";
+  }
+}
