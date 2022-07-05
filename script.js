54ceea0ce648a1ed67b378c2f112a11908137451
JustEddie <skdpwls830@gmail.com>
2022-07-05 13:36:59 +1000

    delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop < 50) {
-    document.getElementById("divHead").style.fontSize = "40px";
-  } else {
-    document.getElementById("divHead").style.fontSize = "20px";
-  }
-}
